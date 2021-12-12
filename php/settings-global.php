569912b1e9734e2ef1bfa92c4b7f618238ae081b
Rafael Nockmann <rafael.nockmann@nocksoft.de>
2021-12-12 13:16:20 +0100

    General settings page optimized

--- php/settings-global.php
+++ php/settings-global.php
@@ -75,20 +75,23 @@ function nstab_globalsettings() {
 				if ($tab == "general" || $tab == null) {
 					?>
 					<table class="form-table">
-					<tr valign="top">
 						<tr valign="top">
-						<th scope="row"><?php echo __("Display Author Box at the end of each post automatically", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_displayauthorboxonposts" <?php if ($nstab_setting_displayauthorboxonposts == true) echo "checked"; ?> /></td>
+						<th scope="row"><?php echo __("Author Box display options", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="checkbox" id="nstab_setting_displayauthorboxonposts" name="nstab_setting_displayauthorboxonposts" <?php if ($nstab_setting_displayauthorboxonposts == true) echo "checked"; ?> />
+							<label for="nstab_setting_displayauthorboxonposts"><?php echo __("Display Author Box at the end of each post automatically", "author-box-by-nocksoft"); ?></label>
+							<br>
+							<input type="checkbox" id="nstab_setting_displayauthorboxonpages" name="nstab_setting_displayauthorboxonpages" <?php if ($nstab_setting_displayauthorboxonpages == true) echo "checked"; ?> />
+							<label for="nstab_setting_displayauthorboxonpages"><?php echo __("Display Author Box at the end of each page automatically (not on front page, blog homepage and privacy policy)", "author-box-by-nocksoft"); ?></label>
+						</td>
 						</tr>
 
 						<tr valign="top">
-						<th scope="row"><?php echo __("Display Author Box at the end of each page automatically (not on front page, blog homepage and privacy policy)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_displayauthorboxonpages" <?php if ($nstab_setting_displayauthorboxonpages == true) echo "checked"; ?> /></td>
-						</tr>
-
-						<tr valign="top">
-						<th scope="row"><?php echo __("Hide the author box of default WordPress theme (tested from Twenty Nineteen up to Twenty Twenty-One)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_hidewordpressauthorbox" <?php if ($nstab_setting_hidewordpressauthorbox == true) echo "checked"; ?> /></td>
+						<th scope="row"><?php echo __("Other options", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="checkbox" id="nstab_setting_hidewordpressauthorbox" name="nstab_setting_hidewordpressauthorbox" <?php if ($nstab_setting_hidewordpressauthorbox == true) echo "checked"; ?> />
+							<label for="nstab_setting_hidewordpressauthorbox"><?php echo __("Hide the author box of default WordPress theme (tested from Twenty Nineteen up to Twenty Twenty-One)", "author-box-by-nocksoft"); ?></label>
+						</td>
 						</tr>
 					</table>
 					<?php
@@ -96,49 +99,60 @@ function nstab_globalsettings() {
 				else if ($tab == "layout") {
 					?>
 					<table class="form-table">
-					<tr valign="top">
-						<th scope="row"><?php echo __("Show Border of Author Box", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_showborder" <?php if ($nstab_setting_showborder == true) echo "checked"; ?> /></td>
+						<tr valign="top">
+						<th scope="row"><?php echo __("Border", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="checkbox" id="nstab_setting_showborder" name="nstab_setting_showborder" <?php if ($nstab_setting_showborder == true) echo "checked"; ?> />
+							<label for="nstab_setting_showborder"><?php echo __("Show Border of Author Box", "author-box-by-nocksoft"); ?></label>
+						</td>
 						</tr>
 
 						<tr valign="top">
-						<th scope="row"><?php echo __("Size of Avatar (Pixel)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="number" name="nstab_setting_avatarsize" min="96" max="200" value="<?php echo $nstab_setting_avatarsize; ?>" /></td>
+						<th scope="row"><?php echo __("Avatar", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="number" id="nstab_setting_avatarsize" name="nstab_setting_avatarsize" min="96" max="200" value="<?php echo $nstab_setting_avatarsize; ?>" />
+							<label for="nstab_setting_avatarsize"><?php echo __("Size of Avatar (Pixel)", "author-box-by-nocksoft"); ?></label>
+							<p>
+							<input type="checkbox" id="nstab_setting_circleavatar" name="nstab_setting_circleavatar" <?php if ($nstab_setting_circleavatar == true) echo "checked"; ?> />
+							<label for="nstab_setting_circleavatar"><?php echo __("Use a circle avatar instead of a square", "author-box-by-nocksoft"); ?></label>
+							</p>
+						</td>
 						</tr>
 
 						<tr valign="top">
-						<th scope="row"><?php echo __("Use a circle avatar instead of a square", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_circleavatar" <?php if ($nstab_setting_circleavatar == true) echo "checked"; ?> /></td>
+						<th scope="row"><?php echo __("Header", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="text" id="nstab_setting_headline" name="nstab_setting_headline" placeholder="<?php echo __("e.g. A Post by", "author-box-by-nocksoft"); ?>" value="<?php echo $nstab_setting_headline; ?>" />
+							<label for="nstab_setting_headline"><?php echo __("Headline", "author-box-by-nocksoft"); ?></label>
+							<p>
+							<input type="number" id="nstab_setting_fontsizeheadline" name="nstab_setting_fontsizeheadline" min="0.5" max="2" step="0.1" value="<?php echo $nstab_setting_fontsizeheadline; ?>" />
+							<label for="nstab_setting_fontsizeheadline"><?php echo __("Fontsize of Headline (em)", "author-box-by-nocksoft"); ?></label>
+							</p>
+							<p>
+							<input type="number" id="nstab_setting_fontsizeposition" name="nstab_setting_fontsizeposition" min="0.4" max="0.9" step="0.1" value="<?php echo $nstab_setting_fontsizeposition; ?>" />
+							<label for="nstab_setting_fontsizeposition"><?php echo __("Fontsize of author's Position (em)", "author-box-by-nocksoft"); ?></label>
+							</p>
+						</td>
 						</tr>
 
 						<tr valign="top">
-						<th scope="row"><?php echo __("Headline", "author-box-by-nocksoft"); ?></th>
-						<td><input type="text" name="nstab_setting_headline" placeholder="<?php echo __("e.g. A Post by", "author-box-by-nocksoft"); ?>" value="<?php echo $nstab_setting_headline; ?>" /></td>
+						<th scope="row"><?php echo __("Biography", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="number" id="nstab_setting_fontsizebio" name="nstab_setting_fontsizebio" min="0.4" max="1.5" step="0.1" value="<?php echo $nstab_setting_fontsizebio; ?>" />
+							<label for="nstab_setting_fontsizebio"><?php echo __("Fontsize of Biography (em)", "author-box-by-nocksoft"); ?></label>
+						</td>
 						</tr>
 
 						<tr valign="top">
-						<th scope="row"><?php echo __("Fontsize of Headline (em)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="number" name="nstab_setting_fontsizeheadline" min="0.5" max="2" step="0.1" value="<?php echo $nstab_setting_fontsizeheadline; ?>" /></td>
-						</tr>
-
-						<tr valign="top">
-						<th scope="row"><?php echo __("Fontsize of author's Position (em)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="number" name="nstab_setting_fontsizeposition" min="0.4" max="0.9" step="0.1" value="<?php echo $nstab_setting_fontsizeposition; ?>" /></td>
-						</tr>
-
-						<tr valign="top">
-						<th scope="row"><?php echo __("Fontsize of Biography (em)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="number" name="nstab_setting_fontsizebio" min="0.4" max="1.5" step="0.1" value="<?php echo $nstab_setting_fontsizebio; ?>" /></td>
-						</tr>
-
-						<tr valign="top">
-						<th scope="row"><?php echo __("Fontsize of Links (em)", "author-box-by-nocksoft"); ?></th>
-						<td><input type="number" name="nstab_setting_fontsizelinks" min="0.3" max="1" step="0.1" value="<?php echo $nstab_setting_fontsizelinks; ?>" /></td>
-						</tr>
-
-						<tr valign="top">
-						<th scope="row"><?php echo __("Display a link to the author's archive", "author-box-by-nocksoft"); ?></th>
-						<td><input type="checkbox" name="nstab_setting_displayauthorsarchive" <?php if ($nstab_setting_displayauthorsarchive == true) echo "checked"; ?> /></td>
+						<th scope="row"><?php echo __("Footer", "author-box-by-nocksoft"); ?></th>
+						<td>
+							<input type="number" id="nstab_setting_fontsizelinks" name="nstab_setting_fontsizelinks" min="0.3" max="1" step="0.1" value="<?php echo $nstab_setting_fontsizelinks; ?>" />
+							<label for="nstab_setting_fontsizelinks"><?php echo __("Fontsize of Links (em)", "author-box-by-nocksoft"); ?></label>
+							<p>
+							<input type="checkbox" id="nstab_setting_displayauthorsarchive" name="nstab_setting_displayauthorsarchive" <?php if ($nstab_setting_displayauthorsarchive == true) echo "checked"; ?> />
+							<label for="nstab_setting_displayauthorsarchive"><?php echo __("Display a link to the author's archive", "author-box-by-nocksoft"); ?></label>
+							</p>
+						</td>
 						</tr>
 					</table>
 					<?php
